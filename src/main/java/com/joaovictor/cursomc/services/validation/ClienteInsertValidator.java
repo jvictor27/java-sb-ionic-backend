82a88d2e854653379def5a40cb8de24298166d89
jvictor27 <jvictor451@gmail.com>
2019-09-06 22:58:14 -0300

    alidacao customizada email não repetido na inserco de Cliente

--- src/main/java/com/joaovictor/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/joaovictor/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.joaovictor.cursomc.domain.Cliente;
 import com.joaovictor.cursomc.domain.enums.TipoCliente;
 import com.joaovictor.cursomc.dto.ClienteNewDTO;
+import com.joaovictor.cursomc.repositories.ClienteRepository;
 import com.joaovictor.cursomc.resources.exceptions.FieldMessage;
 import com.joaovictor.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -22,20 +29,18 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		if (objDto.getTipo() == null) {
-			list.add(new FieldMessage("tipo", "Tipo não pode ser nulo"));
-		}
-		
-		if (objDto.getTipo() == null) {
-			list.add(new FieldMessage("tipo", "Tipo não pode ser nulo"));
-		}
-		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
 		}
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 		
 		for (FieldMessage e : list) {
