9d2c398fce5842b433087dd5e3716a31b4b374cb
jvictor27 <jvictor451@gmail.com>
2019-09-08 17:38:04 -0300

    Busca de produtos por nome e categorias, adicioando anotacao transactional

--- src/main/java/com/joaovictor/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/joaovictor/cursomc/repositories/ProdutoRepository.java
@@ -15,7 +15,8 @@ import com.joaovictor.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	@Transactional(readOnly=true)
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria>categorias, Pageable pageRequest);
 	// Mesmo método só que fazendo uso do padrão de nomes do SpringData, o que automatiza a criação da query da conulta
