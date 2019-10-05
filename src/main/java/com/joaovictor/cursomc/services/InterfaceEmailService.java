5a2a2a60b1b0ebbc0d6b289fe894e6f523f97ce8
jvictor27 <jvictor451@gmail.com>
2019-10-05 12:38:25 -0300

    Esqueci a senha, gera nova senha randomica

--- src/main/java/com/joaovictor/cursomc/services/InterfaceEmailService.java
+++ src/main/java/com/joaovictor/cursomc/services/InterfaceEmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.joaovictor.cursomc.domain.Cliente;
 import com.joaovictor.cursomc.domain.Pedido;
 
 public interface InterfaceEmailService {
@@ -14,4 +15,6 @@ public interface InterfaceEmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
