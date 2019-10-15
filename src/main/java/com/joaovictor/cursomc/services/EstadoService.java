58de5bf3bc2375d2ea4bbab7fd3bb42562f7eb14
jvictor27 <jvictor451@gmail.com>
2019-10-15 18:45:59 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/joaovictor/cursomc/services/EstadoService.java
@@ -0,0 +1,20 @@
+package com.joaovictor.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaovictor.cursomc.domain.Estado;
+import com.joaovictor.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repo;
+
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+}
